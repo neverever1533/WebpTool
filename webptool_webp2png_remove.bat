75d5816af8cfa1eaada5e48238a553cfb6a22b3f
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2026-06-04 11:33:16 +0800

    创建 webptool_webp2png_remove.bat

---
+++ webptool_webp2png_remove.bat
@@ -0,0 +1,40 @@
+@echo off
+if not exist dwebp.exe goto alert0
+@echo off
+if exist dwebp.exe goto ready
+
+:alert0
+echo dwebp.exe does not exist
+goto hold
+
+:ready
+@echo off
+if not exist inWebpFiles.txt goto alert1
+@echo off
+if exist inWebpFiles.txt goto load
+
+:alert1
+echo inWebpFiles.txt does not exist
+echo d:\1.webp > inWebpFiles.txt
+echo d:\2.webp >> inWebpFiles.txt
+goto hold
+
+:load
+for /f "delims=*" %%i in (inWebpFiles.txt) do (
+    if not exist %%i (
+        echo %%i does not exist
+        goto hold
+    )
+    if exist %%i (
+        dwebp "%%i" -o "%%i.png"
+        ::echo WEBP has been converted into a PNG, the webp file has been removed.
+        del %%i
+    )
+)
+
+:hold
+echo Touch any key to continue.
+pause
+
+:end
+exit
